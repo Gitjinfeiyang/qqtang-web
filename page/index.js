b352fb8d0109596289bace20a478de1c73c2a63e
jinfy@ipampas.com <jinfy@ipampas.com>
2020-04-22 14:16:53 +0800

    update

--- page/index.js
+++ page/index.js
@@ -1,7 +1,7 @@
 import io from '../static/socket.io.min.js'
 import game from '../src/game';
 import './main.css'
-const socket = io('https://jinfeiyang.top:3003');
+const socket = io('http://localhost:3003');
 // const socket = io('http://10.10.71.238:3003');
 // var ws = new WebSocket("wss://jinfeiyang.top:3003");
 // ws.onopen=function(){
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3003');
 
 socket.on("connect",function(socket){
     //connect success
